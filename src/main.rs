69aaeb37ac321dea586fa3f0cc5a3d005660295a
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-06-01 15:37:37 +0800

    Update color

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,25 @@ impl Board {
             black: -1,
             white: 1,
         };
-        board.board[3][3] = board.black;
-        board.board[3][4] = board.white;
-        board.board[4][3] = board.white;
-        board.board[4][4] = board.black;
+        board.board[3][3] = board.white;
+        board.board[3][4] = board.black;
+        board.board[4][3] = board.black;
+        board.board[4][4] = board.white;
         board
     }
 }
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        for col in self.board.iter() {
-            for row in col {
+        write!(f, "\x1B[30m\x1B[42m{}\x1B[0m", "   a b c d e f g h \n");
+        for i in 0..self.board.len() {
+            write!(f, "\x1B[30m\x1B[42m{} {} \x1B[0m", "", i + 1);
+            for row in &self.board[i] {
                 match row {
-                    &-1 => write!(f, "\x1B[30m{} \x1B[0m", "●"),
-                    &1 => write!(f, "\x1B[37m{} \x1B[0m", "●"),
-                    _ => write!(f, "\x1B[31m{} \x1B[0m", "-"),
+                    //TODO change -1 to black
+                    &-1 => write!(f, "\x1B[30m\x1B[42m{} \x1B[0m", "●"),
+                    &1 => write!(f, "\x1B[37m\x1B[42m{} \x1B[0m", "●"),
+                    _ => write!(f, "\x1B[42m{} \x1B[0m", "-"),
                 };
             }
             write!(f, "{}", "\n");
