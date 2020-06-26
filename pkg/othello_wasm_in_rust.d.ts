b0d8eea58229a1d5e668032a3afd601e0de0ec01
vn7n24fzkq <vn7n24fzkq@users.noreply.github.com>
2020-06-26 09:37:39 +0000

    deploy: c05bd9cf1ea5b37adbd3e774c6d84585d0fc0da3

--- pkg/othello_wasm_in_rust.d.ts
+++ pkg/othello_wasm_in_rust.d.ts
@@ -25,3 +25,29 @@ export class Othello {
 */
   draw(): void;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_othello_free: (a: number) => void;
+  readonly othello_finish: (a: number) => number;
+  readonly othello_new: () => number;
+  readonly othello_start: (a: number, b: number, c: number) => void;
+  readonly othello_get_winner: (a: number) => number;
+  readonly othello_draw: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
