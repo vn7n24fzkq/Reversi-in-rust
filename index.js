cde25240c7d514bddb306f89b0230edc2f833969
vn7n24fzkq <vn7n24fzkq@users.noreply.github.com>
2020-06-26 10:05:09 +0000

    deploy: d61280574f2787937524a04800e83b0375c9d846

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-import {Othello}  from "./pkg/othello_wasm_in_rust.js";
-
+import {
+    Othello
+} from './pkg/othello_wasm_in_rust.js';
 
 var othello = Othello.new();
-
-function run(){
+async function run() {
     othello.draw();
-    if(!othello.finish()){
-        othello.start(true,true);
-    }else{
-      switch(othello.get_winner()) {
-        case 1:
-            alert("Black WIN!!");
-            break;
-        case -1:
-            alert("White WIN!!");
-            break;
-        default:
-            alert("DRAW!!");
+    if (!othello.finish()) {
+        othello.start(true, true);
+    } else {
+        switch (othello.get_winner()) {
+            case 1:
+                alert("Black WIN!!");
+                break;
+            case -1:
+                alert("White WIN!!");
+                break;
+            default:
+                alert("DRAW!!");
         }
 
         othello = Othello.new();
     }
 }
-
-setInterval(run , 200);
+setInterval(run, 200);
